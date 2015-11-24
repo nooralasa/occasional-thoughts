8aa9f5913c9c2fe5003ff8bf71dde5ee8b2df24d
Suniyya A. Waraich <suniyya@mit.edu>
2015-11-23 20:24:32 -0500

    Changed cover photo for events. Deleted register button and accompanying change. Changed About message...

--- public/javascripts/occasion.js
+++ public/javascripts/occasion.js
@@ -11,6 +11,7 @@ $(document).on('click', '#add-thought', function(evt) {
     evt.preventDefault();
 	var message = document.getElementById('thought-message').value;
 	var id = $("input[name=occasionId]").val();
+	var name = $("input[name=userName]").val();
 	//var photo = document.getElementById().value;
 	//var isPublic = document.getElementById().value;
 	$.post('/occasions/'+id+"/thoughts", 
@@ -18,7 +19,14 @@ $(document).on('click', '#add-thought', function(evt) {
 			photo: "http://cdn.toonvectors.com/images/40/14323/toonvectors-14323-140.jpg",
 			isPublic: true}
 		).done(function(response) {
-          console.log("ajax req sent" );
+          $('#thought-list').append( 
+          	"<div class="thought">
+                   <li >
+                    <img src='http://cdn.toonvectors.com/images/40/14323/toonvectors-14323-140.jpg'>
+                    <h3 align="left">"+ name +"></h3>
+                    <p align= "left" >"+ message +"</p>
+                  </li>
+              </div>");
      	     
       }).fail(function(responseObject) {
           var response = $.parseJSON(responseObject.responseText);
