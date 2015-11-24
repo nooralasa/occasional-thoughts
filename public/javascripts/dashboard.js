76bdbb2855f6efbaf3ca88da2c82cc3f7b503bff
Noor Eddin Amer <nooralasa@gmail.com>
2015-11-23 20:02:05 -0500

    Progress on autocomplete

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -25,10 +25,36 @@ $(function () {
         console.log("The call is being called");
         console.log(data);
     });
-    })
-    
+    })    
   });
-})
+});
+
+$(function () {
+  $.get("/users/current",function (data) {
+    var fbid = data.content.user.fbid;
+    var token = data.content.user.token;
+    console.log(fbid);
+    $.get("https://graph.facebook.com/v2.5/me/friends?access_token="+token, function(obj, status){
+      var friends = [];
+      obj.data.forEach(function (friend, friendIndex) {
+        friends.push(friend.name);
+      });
+      console.log(friends);
+      $('#share').autocomplete({
+        source: friends
+      });
+    });
+  });
+
+  $('#share').keypress(function (e) {
+    if(e.which == 13) {
+      e.preventDefault();
+      console.log($('#share').val());
+    }
+  });
+});
+
+
                       
 
 
